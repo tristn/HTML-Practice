c32b0caadacb33137bf9cce18422e5fc53112f8a
tristn <tristn@users.noreply.github.com>
2022-06-05 20:30:12 -1000

    redo

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
   console.log('ready!');
   $('button').html('Click Me');
   $('button').on('click', function (event) {
-    $('h1').toggleClass('bgswitch');
+    $('h1').toggleClass('cswitch');
   });
 });
