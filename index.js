60e0ae954416e61acf779e95d920bb98e6cd7845
tristn <tristn@users.noreply.github.com>
2022-06-05 21:04:03 -1000

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('button').html('Click Me');
   $('button').on('click', function (event) {
-    $('h1').css('color', 'black');
+    $('#hidden').css('color', 'black');
   });
 });
