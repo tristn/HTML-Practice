25f3c184833f4c06045b0db9c58aaa68f823fa05
tristn <tristn@users.noreply.github.com>
2022-06-05 21:02:06 -1000

    Fixing jquery

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Web Page Example
-
-You can view the webpage by using this HTML viwer: https://htmlpreview.github.io/
+# Bootstrap-Practice
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-  console.log('ready!');
   $('button').html('Click Me');
   $('button').on('click', function (event) {
-    $('h1').toggleClass('cswitch');
+    $('h1').css('color', 'black');
   });
 });
