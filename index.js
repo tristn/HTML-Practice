f0b8a49dab6420b55a2e9c0fd6676dac1e37d4d7
tristn <tristn@users.noreply.github.com>
2022-06-05 21:09:39 -1000

    no sc

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("#hidden").hover(function() {
-        $(this.css("color", "black");
+        $(this.css("color", "black")
     },
     function() {
         $(this).hide();
