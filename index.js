37feb35bd453763c07b950758fefec372a0df9f7
tristn <tristn@users.noreply.github.com>
2022-06-05 20:29:31 -1000

    jquery

---
+++ index.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  console.log('ready!');
+  $('button').html('Click Me');
+  $('button').on('click', function (event) {
+    $('h1').toggleClass('bgswitch');
+  });
+});
