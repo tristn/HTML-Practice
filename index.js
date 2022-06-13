352553ad50911f7e268ee3f0676ed66259087165
tristn <tristn@users.noreply.github.com>
2022-06-12 22:05:29 -1000

    capitalization

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $('button').html('Click me to reveal hidden text!');
-  $('button').on('Click', function (event) {
-    $('hidden').toggleClass('surpriseColor');
+  $('button').on('click', function (event) {
+    $('#hidden').toggleClass('surpriseColor');
   });
 });
